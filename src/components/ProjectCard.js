6dc1362bd4d4088e8520af09f2595bb0da26a7ce
zust_hh <819473021@qq.com>
2019-05-10 15:57:10 +0800

    项目卡片样式优化

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,19 +1,27 @@
 import React from "react";
-import { ImageBackground, Subtitle, Divider, View, Button, Text } from '@shoutem/ui';
+import { ImageBackground, Subtitle, Divider, View, Button, Text, Image, TouchableOpacity } from '@shoutem/ui';
 import { Mutation } from 'react-apollo';
 import gql from '../gql';
+
+const guoImage = require('../static/guo.png');
+const shenImage = require('../static/shen.png');
+const xiaoImage = require('../static/xiao.png');
+const favoriteImage = require('../static/favorite.png')
+const favoriteonImage = require('../static/favoriteon.png')
+
+const typeIcon = [guoImage, shenImage, xiaoImage];
 
 // 收藏之后的更新
 _updateCacheAfterCollection = (store, projectId) => {
   const data = store.readQuery({
-      query: gql.PROJECTLIST_QUERY,
-      variables: { attribution: 0 },
+    query: gql.PROJECTLIST_QUERY,
+    variables: { attribution: 0 },
   })
 
   const favoritedProject = data.projectList.projects.find(project => project.id === projectId)
   data.projectList.myFavoriteProjects = [...data.projectList.myFavoriteProjects, {
-      id: projectId,
-      __typename: "Project"
+    id: projectId,
+    __typename: "Project"
   }]
   favoritedProject.showFavorite = false
   store.writeQuery({ query: gql.PROJECTLIST_QUERY, data })
@@ -29,18 +37,20 @@ const ProjectCard = (data) => {
         {
           data.length !== 0 ? <View styleName="space-between vertical fill-parent" style={{ backgroundColor: 'rgba(0,0,0,0.2)', padding: 24 }}>
             <View styleName="horizontal space-between">
-              <Text>{data.type}</Text>
+              <Image
+                style={{ width: 24, height: 24 }}
+                source={typeIcon[data.type - 1]}
+              />
               <View>
-                <Text style={{ color: 'white' }}>{data.addBy.name}</Text>
+                <Text style={{ color: 'white', fontSize: 18 }}>{data.addBy.name}</Text>
               </View>
             </View>
             <View styleName="horizontal space-between">
               <View styleName="vertical">
                 <View styleName="horizontal">
-                  <Text numberOfLines={2} style={{ color: 'white', width: 300 }}>{data.name}</Text>
-                  <Text style={{ color: 'white' }}>{data.tutor.name}</Text>
+                  <Text numberOfLines={2} style={{ color: 'white', width: 300, fontSize: 20, fontWeight: 'bold' }}>{data.name}</Text>
                 </View>
-                <Subtitle styleName="sm-gutter-horizontal" style={{ color: 'white' }}>{data.description}</Subtitle>
+                <Subtitle styleName="sm-gutter-horizontal" style={{ color: 'rgb(200,200,200)' }}>{data.description}</Subtitle>
               </View>
               {
                 data.showFavorite ? <Mutation
@@ -51,13 +61,16 @@ const ProjectCard = (data) => {
                   }
                 >
                   {mutation => (
-                    <Button
+                    <TouchableOpacity
                       onPress={mutation}
                     >
-                      <Text>收藏</Text>
-                    </Button>
+                      <Image
+                        style={{ height: 30, width: 30 }}
+                        source={favoriteImage}
+                      />
+                    </TouchableOpacity>
                   )}
-                </Mutation> : null
+                </Mutation> : <Image style={{ height: 30, width: 30 }} source={favoriteonImage} />
               }
             </View>
           </View> : null
